a4246be6fd1c38ea392f69921f306fa9cc39ad7a
Fran <fjvica8@gmail.com>
2022-10-14 12:32:38 +0200

    terminate code and create Docker file

--- msvc-cursos/src/main/java/org/springcloud/msvc/cursos/msvc/cursos/repositories/CursoRepository.java
+++
@@ -1,7 +0,0 @@
-package org.springcloud.msvc.cursos.msvc.cursos.repositories;
-
-import org.springcloud.msvc.cursos.msvc.cursos.entity.CursoEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CursoRepository extends CrudRepository<CursoEntity, Long> {
-}
--- msvc-usuarios/src/main/java/com/springcluod/msvc/usuarios/msvc/usuarios/repositories/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springcluod.msvc.usuarios.msvc.usuarios.repositories;
-
-import com.springcluod.msvc.usuarios.msvc.usuarios.models.entity.Usuario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
-}
